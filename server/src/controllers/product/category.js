7efa4c31f731eea0068cc280b5bcf9e540952533
numdinkushi <numdinkushi@gmail.com>
2025-01-21 09:20:58 +0100

    get category by id

--- server/src/controllers/product/category.js
+++ server/src/controllers/product/category.js
@@ -10,3 +10,19 @@ export const getAllCategories = async (req, reply) => {
         reply.status(500).send({ message: "Error retrieving categories" });
     }
 };
+
+export const getCategoryById = async (req, reply) => {
+    try {
+        const { id } = req.params;
+        const category = await Category.findById(id);
+
+        if (!category) {
+            return reply.status(404).send({ message: "Category not found" });
+        }
+
+        return reply.send(category);
+    } catch (error) {
+        console.error(error);
+        reply.status(500).send({ message: "Error retrieving category" });
+    }
+};
