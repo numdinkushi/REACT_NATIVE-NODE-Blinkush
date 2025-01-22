4963d9c1483fb638bd8d0fc78d23c50cd8eb7281
numdinkushi <numdinkushi@gmail.com>
2025-01-22 16:13:46 +0100

    added category filter

--- server/src/controllers/product/category.js
+++ server/src/controllers/product/category.js
@@ -1,9 +1,9 @@
-import  Category  from "../../models/category.js";
+import Category from "../../models/category.js";
+import mongoose from "mongoose";
 
 export const getAllCategories = async (req, reply) => {
     try {
         const categories = await Category.find();
-        
         return reply.send(categories);
     } catch (error) {
         console.error(error);
@@ -24,5 +24,30 @@ export const getCategoryById = async (req, reply) => {
     } catch (error) {
         console.error(error);
         reply.status(500).send({ message: "Error retrieving category" });
+    }
+};
+
+export const getCategories = async (req, reply) => {
+    try {
+        const { startId, endId } = req.query;
+
+        let query = {};
+
+        // Check if startId and endId are provided
+        if (startId && endId) {
+            // Convert startId and endId to ObjectId
+            const startObjectId = new mongoose.Types.ObjectId(startId);
+            const endObjectId = new mongoose.Types.ObjectId(endId);
+
+            // Set the query to find categories within the specified range
+            query._id = { $gte: startObjectId, $lte: endObjectId };
+        }
+
+        const categories = await Category.find(query);
+
+        return reply.send(categories);
+    } catch (error) {
+        console.error(error);
+        reply.status(500).send({ message: "Error retrieving categories" });
     }
 };
